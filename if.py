95731aff9a1a49940139fbbcd04242310166228d
azak <zakidawud69@gmail.com>
2025-12-14 13:26:51 +0700

    Dasar

--- if.py
+++ if.py
@@ -2,9 +2,9 @@ angka = int(input("masukkan angka = "))
 hasil = "besar" if angka > 10 else "kecil"
 print(hasil)
 
-if hasil == "besar":
+if hasil == "besar": # menggunakan if untuk mengecek kondisi
     print("angka lebih dari 10")
-else:
+else: # menggunakan else untuk kondisi sebaliknya
     print("angka 10 atau kurang")
 
-# Program Percabangan Sederhana
+# Program ini mendemonstrasikan penggunaan pernyataan if-else di Python.
