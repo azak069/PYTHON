3172679d13911d0fba313d9de5adf50ec4cff400
azak <zakidawud69@gmail.com>
2025-12-14 13:23:21 +0700

    Dasar

--- function.py
+++ function.py
@@ -1,7 +1,7 @@
 def fungsi_sapa(nama):
-    print(f"Halo, {nama}!")
+    print(f"Halo, {nama}!") # membuat fungsi untuk menyapa seseorang dengan parameter nama
 
-fungsi_sapa("Andi")
+fungsi_sapa("Andi") # memanggil fungsi_sapa dengan argumen "Andi"
 fungsi_sapa("Budi")
 
 # Program Fungsi Sederhana
@@ -9,16 +9,16 @@ fungsi_sapa("Budi")
 def luas_lingkaran(radius):
     phi = 3.14
     luas = phi * radius * radius
-    return luas
+    return luas # return digunakan untuk mengembalikan nilai dari fungsi agar bisa digunakan di luar fungsi
 
 hasil_luas = luas_lingkaran(7)
 print(f"Luas lingkaran dengan radius 7 adalah {hasil_luas}")
 
 # Program Fungsi dengan Return Value
 
-def terimkasih(nama, makasih="terima kasih"):
+def terimkasih(nama, makasih="terima kasih"): 
     print(f"{makasih}, {nama}!")
-
+# membuat fungsi dengan argumen default, yaitu argumen yang ditetapkan nilainya jika tidak diberikan saat pemanggilan fungsi
 terimkasih("Siti")
 terimkasih("Ani", "Thanks a lot")
 
@@ -30,15 +30,15 @@ def profil(nama, umur, kota="Jogja", pekerjaan="Pelajar"):
 profil("budi", 21)
 profil("susi", 25, kota="Jakarta", pekerjaan="Programmer")
 profil("andi", 30, pekerjaan="Designer")
-
+# memanggil fungsi dengan berbagai kombinasi argumen posisi dan argumen kata kunci
 # Program Fungsi dengan Argumen Nama
 
-nama = "Joko"
+nama = "Joko" # variabel global
 def nama():
     print("Nama saya : ", nama)
 
 def ubah_nama():
-    global nama
+    global nama # menggunakan kata kunci global untuk mengubah variabel global di dalam fungsi
     nama = "Putra"
     print("Nama saya diubah menjadi : ", nama)
 
@@ -47,7 +47,7 @@ nama()
 
 # Program Fungsi dan Variabel Global
 
-def cetak_list(*data):
+def cetak_list(*data): # menggunakan tanda asterisk (*) untuk argumen dalam bentuk tuple
     for item in data:
         print(item)
 
@@ -56,7 +56,7 @@ cetak_list("Saya", "Aku", "Gue", "Nyong")
 
 # Program Fungsi list dengan Argumen Tak Terbatas
 
-def cetak_dict(**isi):
+def cetak_dict(**isi): # menggunakan dua tanda 2 asterisk (**) untuk argumen dalam bentuk dictionary
     for key, value in isi.items():
         print(f"{key} : {value}")
 
@@ -64,3 +64,5 @@ cetak_dict(nama="Saya", nama2="Aku", nama3="Gue")
 cetak_dict(umur=20, umur2=22, umur3=25)
 
 # Program Fungsi dictionary dengan Argumen Tak Terbatas
+
+# Program ini mendemonstrasikan penggunaan fungsi di Python.
