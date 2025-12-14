053afd48893da42ce036f8bd5a96a960650ec19f
azak <zakidawud69@gmail.com>
2025-12-14 18:55:54 +0700

    Dasar

--- while.py
+++ while.py
@@ -1,7 +1,7 @@
 password_true = "etcode99"
 percobaan = 0
 max_percobaan = 3
-
+# menggunakan loop while yang akan terus berjalan selama percobaan kurang dari maksimal percobaan
 while percobaan < max_percobaan:
     password_input = input("Masukkan password: ")
 
@@ -11,8 +11,8 @@ while percobaan < max_percobaan:
     else:
         percobaan += 1
         print(f"Password salah. Percobaan ke-{percobaan} dari {max_percobaan}.")
-
+# jika percobaan telah mencapai maksimal, tampilkan pesan akses ditolak di luar loop
 else:
     print("Akses ditolak. Terlalu banyak percobaan gagal.")
 
-# Program Validasi Password
+# Program ini mendemonstrasikan penggunaan loop while untuk membatasi jumlah percobaan memasukkan password di Python.
