fd44910020bc1df4b0ba2abf46517f26c0d2e12d
azak <zakidawud69@gmail.com>
2025-12-14 19:14:51 +0700

    Dasar

---
+++ tuple.py
@@ -0,0 +1,8 @@
+angka = (5, 2, 9, 1, 5, 6) # membuat tuple dengan beberapa angka
+print(angka[0])  # Mengakses elemen pertama dari tuple
+print(angka[-1]) # Mengakses elemen terakhir dari tuple
+# tuple bersifat immutable, sehingga elemen tidak dapat diubah
+# angka[0] = 10  # Ini akan menghasilkan error karena tuple tidak dapat diubah
+print(len(angka)) # Menghitung panjang tuple
+
+# Program ini mendemonstrasikan penggunaan tuple di Python.
