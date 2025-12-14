639f99083f7d18f34ff0c3c03aa5b078849c992f
azak <zakidawud69@gmail.com>
2025-12-14 13:29:55 +0700

    Dasar

---
+++ input_output.py
@@ -0,0 +1,8 @@
+nama = input("Masukkan nama: ") # mengambil input nama dari pengguna
+umur = input("Masukkan umur: ") # mengambil input umur dari pengguna
+
+print("Nama Saya " + nama + ", umur " + umur) # menampilkan nama dan umur yang dimasukkan
+sisa_umur = 100 - int(umur) # menghitung sisa umur dengan mengurangi umur dari 100
+print("Sisa umur saya " + str(sisa_umur)) # menampilkan sisa umur dalam bentuk string
+
+# Program ini mendemonstrasikan penggunaan input dan output dasar di Python.
