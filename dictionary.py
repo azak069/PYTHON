bef25e3698e0e8c27b46cd9fc1b9d22f0c6ecfcb
azak <zakidawud69@gmail.com>
2025-12-14 19:19:02 +0700

    Dasar

--- dictionary.py
+++ dictionary.py
@@ -1,5 +1,18 @@
-data = {1 : "zaki",2 : 22,3 : "Magelang"}
+profile = {
+    "nama": "zaki",
+    "umur": 20,
+    "asal": "Magelang"
+} # membuat dictionary dengan beberapa pasangan kunci-nilai
 
+profile["pekerjaan"] = "programmer"  # menambahkan pasangan kunci-nilai baru
+profile["umur"] = 18  # mengubah nilai berdasarkan kunci
+print(profile["nama"])  # mengakses nilai berdasarkan kunci
+print(profile.get("asal"))  # mengakses nilai menggunakan metode get()
+profile.pop("pekerjaan")  # menghapus pasangan kunci-nilai berdasarkan kunci
+print(profile)
+
+data = {1 : "zaki",2 : 22,3 : "Magelang"}
+# mengakses dan menampilkan nilai dari dictionary dengan format tertentu
 print(f"Nama saya : {data[1]}. Umur : {data[2]}. Asal : {data[3]}.")
 
 # Program ini mendemonstrasikan penggunaan dictionary di Python.
