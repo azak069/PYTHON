5e2e2a9a2128fb5ce2d108d793e981c9f4b3b6b5
azak <zakidawud69@gmail.com>
2025-12-14 18:50:28 +0700

    Dasar

--- set.py
+++ set.py
@@ -1,5 +1,5 @@
-hobi = {"membaca", "menulis", "berkebun"}
-print(hobi)
+hobi = {"membaca", "menulis", "berkebun"} # membuat set
+print(hobi) # menampilkan seluruh elemen dalam set
 
 hobi.add("bersepeda") # menambahkan elemen baru
 print(hobi)
@@ -8,4 +8,6 @@ hobi.remove("menulis") # menghapus elemen
 print(hobi)
 
 for e in hobi:
-    print(f"Saya suka {e}") # mengiterasi elemen set
+    print(f"Saya suka {e}") # mengiterasi setiap elemen dalam set
+
+# Program ini mendemonstrasikan operasi dasar pada set di Python.
